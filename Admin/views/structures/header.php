3dd82aeb636a4e45f123983a2ce2b9a62f92b9df
Cyrron Fabraquel <cyfabraquel0813@gmail.com>
2024-08-29 16:38:47 +0800

    fixed bug sidebar

--- Admin/views/structures/header.php
+++ Admin/views/structures/header.php
@@ -11,7 +11,6 @@
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="../src/output.css">
-  <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 
 </head>
 
