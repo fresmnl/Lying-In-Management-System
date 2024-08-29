fe453bf9f2b10769e2490c324a6f297d9c2d45b8
Fres Manalo <fresmnl@gmail.com>
2024-08-29 12:42:59 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,24 @@
 module.exports = {
   content: [
     "./Admin/**/*.{html,js,php}",
-    "./Login/**/*.{html,js,php}"
+    "./Login/**/*.{html,js,php}",
+    "./*.{html,js,php}"
   ],
   theme: {
+      screens: {
+        lg: {
+          max: "1200px",
+        },
+        mq1050: {
+          raw: "screen and (max-width: 1050px)",
+        },
+        mq750: {
+          raw: "screen and (max-width: 750px)",
+        },
+        mq450: {
+          raw: "screen and (max-width: 450px)",
+        },
+      },
     extend: {},
   },
   plugins: [],
