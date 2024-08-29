55b1bd28cc8827d1c93e1522660678330f52ebe0
Cyrron Fabraquel <cyfabraquel0813@gmail.com>
2024-08-29 16:43:43 +0800

    restore dashboard for admin panel

--- Admin/views/structures/header.php
+++ Admin/views/structures/header.php
@@ -11,6 +11,9 @@
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="../src/output.css">
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Lato:wght@500;600;700;800;900&display=swap" />
 
 </head>
 
